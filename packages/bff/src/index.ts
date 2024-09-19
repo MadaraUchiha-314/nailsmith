16467568f9d4df159de0594b5bf3c0f8bfb6d018
MadaraUchiha-314 <rohithr31+github@gmail.com>
2024-09-18 20:32:35 -0700

    feat: simple express app

--- packages/bff/src/index.ts
+++ packages/bff/src/index.ts
@@ -1,1 +1,16 @@
-export function run() {}
+import express from 'express'
+
+export function run() {
+  const app = express()
+  const port = 3000
+
+  app.get('/', (_, res) => {
+    res.send('Hello World!')
+  })
+
+  app.listen(port, () => {
+    console.log(`nailsmith:bff listening on port ${port}`)
+  })
+}
+
+run()
